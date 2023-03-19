8852ce588d0ff8f6fd813db1e04af015f03c0dbc
Krimothiazine <krimouv1995@gmail.com>
2023-03-19 16:16:38 +0100

    Implement drop down menu functionality

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// no js here see the other branch
+function classRemover(elements, clss) {
+  elements.forEach((item) => {
+    item.classList.remove(clss);
+  });
+}
+
+function dropDownMenuHandler(
+  navbar,
+  navItems,
+  triggerEventType,
+  leaveEventType,
+  clss
+) {
+  navItems.forEach((item) => {
+    item.addEventListener(`${triggerEventType}`, () => {
+      classRemover(navItems, clss);
+      item.classList.add(clss);
+    });
+  });
+  document.addEventListener(`${leaveEventType}`, (e) => {
+    if (!e.target.matches(`${navbar.tagName} *`)) {
+      classRemover(navItems, clss);
+    }
+  });
+}
+const parent = document.querySelectorAll('.main-bar li');
+const subParent = document.querySelectorAll('.drop-down-menu li');
+const navbar = document.querySelector('nav');
+
+dropDownMenuHandler(navbar, parent, 'mouseover', 'click', 'active');
+dropDownMenuHandler(navbar, subParent, 'mouseover', 'click', 'active-sub');
